7272ac8131ed9a7d610aaee11a853d36b090e878
Jahanvi Chaudhary <91944643+s0ur-oranges@users.noreply.github.com>
2023-08-31 15:57:04 +0530

    Update asgi.py

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -15,6 +15,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
